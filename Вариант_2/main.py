b41f99235033f8db0261ff655fd2fd3d27e81e17
Даша <darakulkina138@gmail.com>
2025-10-11 15:05:59 +0300

    Create main.py

---
+++ Вариант_2/main.py
@@ -0,0 +1,9 @@
+text = input("Введите 10 слов: ")
+words = text.split()
+if len(words) < 10:
+    text = "один два три четыре пять шесть семь восемь девять десять"
+elif len(words) > 10:
+    words = words[:10]
+    text = " ".join(words)
+uppercase_text = text.upper()
+print(uppercase_text)
