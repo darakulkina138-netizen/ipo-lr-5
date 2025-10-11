d14fcf4c6f879ffaba8d7b9c78b60e6ea9552a79
Даша <darakulkina138@gmail.com>
2025-10-11 14:57:58 +0300

    Create main.py

---
+++ Общие/task_1/main.py
@@ -0,0 +1,16 @@
+def analyze_string():
+    text = input("Введите строку на русском языке: ")
+    length = len(text)
+    print(f"Длина строки: {length}")
+    vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯ"
+    vowel_count = 0
+    consonant_count = 0
+    for char in text:
+        if char.isalpha() and ('а' <= char <= 'я' or 'А' <= char <= 'Я' or char in 'ёЁ'):
+            if char in vowels:
+                vowel_count += 1
+            else:
+                consonant_count += 1
+    print(f"Количество гласных: {vowel_count}")
+    print(f"Количество согласных: {consonant_count}")
+analyze_string()
